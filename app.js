23b3a6c69c2d61fc5d9dc180d42d098e33a00780
GlebZhidovich <gzhidovich@gmail.com>
2025-04-20 19:51:30 +0300

    feat: add websockets

--- app.js
+++ app.js
@@ -1,11 +1,41 @@
+const http = require("http");
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const server = http.createServer();
 
 app.get("/", (req, res) => {
   res.sendFile("index.html", { root: __dirname });
 });
 
-app.listen(PORT, () => {
+server.on("request", app);
+server.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
+
+// WebSocket
+
+const WebSocketServer = require("ws").Server;
+
+const wss = new WebSocketServer({ server });
+
+wss.on("connection", function connection(ws) {
+  const numClients = wss.clients.size;
+  console.log("Clients connected:", numClients);
+
+  wss.broadcast(`Current visitors: ${numClients}`);
+
+  if (ws.readyState === ws.OPEN) {
+    ws.send("Welcome to my server");
+  }
+
+  ws.on("close", function close() {
+    console.log("A client has disconnected:");
+  });
+});
+
+wss.broadcast = (data) => {
+  wss.clients.forEach((client) => {
+    client.send(data);
+  });
+};
